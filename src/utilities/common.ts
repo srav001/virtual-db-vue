d4c24b0f0b1c45d047fcabbd95038a5fbd229685
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-08-10 20:57:38 +0530

    Update common.ts

--- src/utilities/common.ts
+++ src/utilities/common.ts
@@ -1,6 +1,4 @@
 // A set of utility functions to use to remove repetitive and widley used code
-import { stringify, parse } from 'devalue';
-
 export type UtilityFunction = (...args: any) => any;
 export type NonUndefined<T> = T extends undefined ? never : T;
 
@@ -28,48 +26,6 @@ export const wrapInTryCatch = <FunctionType>(func: UtilityFunction): FunctionTyp
 export function deepClone<InferedType>(value: InferedType): InferedType {
   return wrapInTryCatch(function () {
     return JSON.parse(JSON.stringify(value));
-  });
-}
-
-/**
- * Same as deepClone but also clones Map or Set. Is also slower comparatively
- * @param value - The value to be cloned.
- */
-export function deepCloneNew<InferedType>(value: InferedType): InferedType {
-  return wrapInTryCatch(function () {
-    return parse(stringify(value));
-  });
-}
-
-interface TypeMap {
-  [key: string]: UtilityFunction;
-}
-
-const typeMap: TypeMap = {
-  'Field.Real': (value: string) => parseFloat(value),
-  'Field.Integer': (value: string) => parseInt(value),
-  'Field.Binary': (value: string) => value.toLowerCase() === 'true',
-  'Field.DateTime': (value: string) => new Date(value).toISOString()
-};
-
-/**
- * It takes a type and a value, and returns the value converted to the type
- * @example src/config/scope-types.ts
- * @param {string} type - The type of the parameter.
- * @param {string} value - The value of the parameter.
- * @returns The type parsed value if it exits in map else the passed value.
- */
-export function parseType(type: string, value: string, exludedTypes: string[] = []) {
-  return wrapInTryCatch(() => {
-    if (exludedTypes.length > 0) {
-      if (!exludedTypes.includes(type) && typeMap[type]) {
-        return typeMap[type](value);
-      }
-    } else if (typeMap[type]) {
-      return typeMap[type](value);
-    }
-
-    return value;
   });
 }
 
@@ -134,33 +90,4 @@ export function get<T>(objectToRead: BasicObject, path: string) {
  */
 export async function sleep(time: number) {
   return new Promise((r) => setTimeout(r, time));
-}
-
-/**
- * Returns the boolean value irrespective of string or boolean
- * @param {boolean | string} val - value to evaluate
- */
-export function getBoolean(val: boolean | string) {
-  switch (typeof val) {
-    case 'boolean':
-      return val;
-    case 'string':
-      return val.toLowerCase() === 'true';
-    default:
-      return false;
-  }
-}
-
-/**
- * Returns the number value irrespective of string or number
- * @param {number | string} val - value to evaluate
- */
-export function getNumber(val: number | string, initial = 1) {
-  if (typeof val === 'number') {
-    return val;
-  } else if (typeof val === 'string') {
-    return parseInt(val);
-  }
-
-  return initial;
 }
